e8e99cce4f2fd2962f677e459860a0ef1da21d7d
byron <3447063@qq.com>
2018-05-14 20:32:19 +0800

    1

---
+++ src/PCME.Api/Application/Commands/ExamSubjectCrateOrUpdateCommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PCME.Domain.AggregatesModel.ExamSubjectAggregates;
+using PCME.Domain.SeedWork;
+using PCME.Infrastructure;
+
+namespace PCME.Api.Application.Commands
+{
+	public class ExamSubjectCrateOrUpdateCommandHandler:IRequestHandler<ExamSubjectCreateOrUpdateCommand,ExamSubject>
+    {
+		
+		private readonly IUnitOfWork<ApplicationDbContext> unitOfWork;
+		private readonly IRepository<ExamSubject> examSubjectRepository;
+
+		public ExamSubjectCrateOrUpdateCommandHandler(IUnitOfWork<ApplicationDbContext> unitOfWork)
+        {
+			this.unitOfWork = unitOfWork;
+			examSubjectRepository = this.unitOfWork.GetRepository<ExamSubject>();
+        }
+
+		public async Task<ExamSubject> Handle(ExamSubjectCreateOrUpdateCommand request, CancellationToken cancellationToken)
+		{
+			var idIsExisted = await examSubjectRepository.FindAsync(request.Id);
+         
+
+            if (idIsExisted == null)//新增单位
+            {
+                var examSubject = new ExamSubject(
+                       request.Code
+                    ,request.Name
+					,request.OpenTypeId
+					,request.ExamTypeId
+					,request.SeriesId
+					,request.ExamSubjectStatusId
+                    ,request.MSCount
+                    ,request.CreditHour
+                   );
+                try
+                {
+					await examSubjectRepository.InsertAsync(examSubject);
+                    await unitOfWork.SaveEntitiesAsync();
+
+                    return await GetShow(examSubject);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+            else//更新单位
+            {
+				idIsExisted.Update(request.Code
+                    , request.Name
+                    , request.OpenTypeId
+                    , request.ExamTypeId
+                    , request.SeriesId
+                    , request.ExamSubjectStatusId
+                    , request.MSCount
+                    , request.CreditHour);
+				examSubjectRepository.Update(idIsExisted);
+                await unitOfWork.SaveEntitiesAsync();
+                return await GetShow(idIsExisted);
+            }
+		}
+		private async Task<ExamSubject> GetShow(ExamSubject examSubject){
+			var result = await examSubjectRepository.Query(c => c.Id == examSubject.Id)
+											  .Include(s => s.ExamSubjectStatus)
+											  .Include(s => s.ExamType)
+											  .Include(s => s.OpenType)
+			                                  .Include(s => s.Series).FirstOrDefaultAsync();
+			return result;
+		}
+	}
+}
---
+++ src/PCME.Api/Application/Commands/ExamSubjectCreateOrUpdateCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Newtonsoft.Json;
+using PCME.Domain.AggregatesModel.ExamSubjectAggregates;
+using PCME.Domain.AggregatesModel.ProfessionalTitleAggregates;
+
+namespace PCME.Api.Application.Commands
+{
+	public class ExamSubjectCreateOrUpdateCommand:IRequest<ExamSubject>
+    {
+		
+		public int Id { get; private set; }
+		/// <summary>
+        /// 科目代码
+        /// </summary>
+        /// <value>The code.</value>
+        [Required(ErrorMessage ="科目代码必须填写")]
+        public string Code
+        {
+            get;
+            private set;
+        }
+		[Required(ErrorMessage = "科目名称必须填写")]
+        public string Name { get; private set; }
+        public OpenType OpenType { get; private set; }
+        public ExamType ExamType { get; private set; }
+        public ExamSubjectStatus ExamSubjectStatus { get; private set; }
+        public Series Series { get; private set; }
+
+		[Required(ErrorMessage = "科目类型必须选择")]
+		[JsonProperty("OpenType.Id")]
+        public int OpenTypeId { get; private set; }
+
+		[Required(ErrorMessage = "考试类型必须选择")]
+		[JsonProperty("ExamType.Id")]
+        public int ExamTypeId { get; private set; }
+
+		[JsonProperty("Series.Id")]
+        public int? SeriesId { get; private set; }
+
+		[Required(ErrorMessage = "科目状态必须选择")]
+		[JsonProperty("ExamSubjectStatus.Id")]
+        public int ExamSubjectStatusId { get; private set; }
+
+        /// <summary>
+        /// 面授次数
+        /// </summary>
+        /// <value>The MSC ount.</value>
+		[Required(ErrorMessage = "面授次数必须填写")]
+        public int MSCount { get; private set; }
+        /// <summary>
+        /// 开设系列
+        /// </summary>
+        /// <value>The series identifier.</value>
+		[Required(ErrorMessage = "学分必须填写")]
+        public int CreditHour { get; private set; }
+
+		public ExamSubjectCreateOrUpdateCommand(int id,string code, string name, int openTypeId, int examTypeId, int? seriesId, int examSubjectStatusId, int mSCount, int creditHour)
+		{
+			Id = id;
+			Code = code;
+			Name = name;
+			OpenTypeId = openTypeId;
+			ExamTypeId = examTypeId;
+			SeriesId = seriesId;
+			ExamSubjectStatusId = examSubjectStatusId;
+			MSCount = mSCount;
+			CreditHour = creditHour;
+		}
+	}
+}
